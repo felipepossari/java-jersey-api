6635b32759c2af1c6b55759d8bd5fb03f4499d3e
Felipe Possari <fcpfelipe@hotmail.com>
2021-11-29 06:11:21 -0300

    Add constants class

---
+++ src/main/java/com/felipepossari/jersey/adapter/AdapterConstans.java
@@ -0,0 +1,9 @@
+package com.felipepossari.jersey.adapter;
+
+public class AdapterConstans {
+    private AdapterConstans() {
+    }
+
+    public static final String ENDPOINT_FOOD = "/foods";
+    public static final String ENDPOINT_BY_ID_FOOD = "/foods/{id}";
+}
