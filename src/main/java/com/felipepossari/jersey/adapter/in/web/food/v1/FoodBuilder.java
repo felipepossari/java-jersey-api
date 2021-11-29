f2bfe1564eedab37041d4068ff244bf47d7688e3
Felipe Possari <fcpfelipe@hotmail.com>
2021-11-29 06:11:46 -0300

    Add food controller

---
+++ src/main/java/com/felipepossari/jersey/adapter/in/web/food/v1/FoodBuilder.java
@@ -0,0 +1,60 @@
+package com.felipepossari.jersey.adapter.in.web.food.v1;
+
+import com.felipepossari.jersey.adapter.in.web.food.v1.request.FoodRequest;
+import com.felipepossari.jersey.adapter.in.web.food.v1.request.FoodTypeRequest;
+import com.felipepossari.jersey.adapter.in.web.food.v1.response.FoodResponse;
+import com.felipepossari.jersey.adapter.in.web.food.v1.response.FoodTypeResponse;
+import com.felipepossari.jersey.application.domain.Food;
+import com.felipepossari.jersey.application.domain.FoodType;
+import org.jvnet.hk2.annotations.Service;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.felipepossari.jersey.adapter.AdapterConstans.ENDPOINT_FOOD;
+
+@Service
+public class FoodBuilder {
+
+    public Food buildFood(FoodRequest request) {
+        return Food.builder()
+                .name(request.getName())
+                .weight(request.getWeight())
+                .type(buildFoodType(request.getType()))
+                .build();
+    }
+
+    public URI buildCreatedUri(Food food) {
+        return URI.create(ENDPOINT_FOOD + "/" + food.getId());
+    }
+
+    public List<FoodResponse> buildResponse(List<Food> foods) {
+        return foods.stream()
+                .map(this::buildResponse)
+                .collect(Collectors.toList());
+    }
+
+    private FoodResponse buildResponse(Food food) {
+        return FoodResponse.builder()
+                .id(food.getId())
+                .name(food.getName())
+                .type(buildFoodTypeResponse(food.getType()))
+                .weight(food.getWeight())
+                .build();
+    }
+
+    private FoodTypeResponse buildFoodTypeResponse(FoodType type) {
+        return FoodTypeResponse.builder()
+                .id(type.getId())
+                .type(type.getType())
+                .build();
+    }
+
+    private FoodType buildFoodType(FoodTypeRequest type) {
+        return FoodType.builder()
+                .id(type.getId())
+                .type(type.getType())
+                .build();
+    }
+}
---
+++ src/main/java/com/felipepossari/jersey/adapter/in/web/food/v1/FoodController.java
@@ -0,0 +1,56 @@
+package com.felipepossari.jersey.adapter.in.web.food.v1;
+
+import com.felipepossari.jersey.adapter.in.web.food.v1.request.FoodRequest;
+import com.felipepossari.jersey.application.domain.Food;
+import com.felipepossari.jersey.application.port.in.CreateFoodUseCase;
+import com.felipepossari.jersey.application.port.in.ReadFoodUseCase;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@Path("/foods")
+public class FoodController {
+
+    private static final Logger log = LoggerFactory.getLogger(FoodController.class);
+    private final FoodBuilder builder;
+    private final CreateFoodUseCase createFoodUseCase;
+    private final ReadFoodUseCase readFoodUseCase;
+
+    @Inject
+    public FoodController(FoodBuilder builder,
+                          CreateFoodUseCase createFoodUseCase,
+                          ReadFoodUseCase readFoodUseCase) {
+        this.builder = builder;
+        this.createFoodUseCase = createFoodUseCase;
+        this.readFoodUseCase = readFoodUseCase;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response postFood(FoodRequest foodRequest) {
+        log.info("Creating food");
+        Food food = builder.buildFood(foodRequest);
+        food = createFoodUseCase.create(food);
+        log.info("Food created. Id: {}", food.getId());
+        return Response.created(builder.buildCreatedUri(food))
+                .build();
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        List<Food> foods = readFoodUseCase.readAll();
+        return Response.ok(builder.buildResponse(foods))
+                .build();
+    }
+}
