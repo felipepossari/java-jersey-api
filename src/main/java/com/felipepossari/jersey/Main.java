afff76346b34dffbc632aebcd553e1c4f461f12e
Felipe Possari <fcpfelipe@hotmail.com>
2021-11-29 20:48:18 -0300

    Update main class

--- src/main/java/com/felipepossari/jersey/Main.java
+++ src/main/java/com/felipepossari/jersey/Main.java
@@ -6,46 +6,33 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.IOException;
 import java.net.URI;
+import java.util.logging.Logger;
 
-/**
- * Main class.
- */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
+    private static final Logger log = Logger.getLogger(Main.class.getName());
     public static final String BASE_URI = "http://localhost:8080/";
+    public static final String BASE_PACKAGE = "com.felipepossari.jersey";
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     *
-     * @return Grizzly HTTP server.
-     */
     public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.felipepossari.jersey package
         final ResourceConfig rc = new ResourceConfig();
-        rc.packages("com.felipepossari.jersey");
+        rc.packages(BASE_PACKAGE);
         rc.register(AutoScanFeatureConfig.class);
         rc.register(LoggingFeature.class);
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
-    /**
-     * Main method.
-     *
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with endpoints available at "
-                + "%s%nHit Ctrl-C to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+        Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+        log.info("Jersey app started with endpoints available at " + BASE_URI);
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            log.severe("Thread join error. Exiting application");
+            Thread.currentThread().interrupt();
+        }
     }
 }
 
