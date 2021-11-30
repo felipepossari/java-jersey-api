7c8729bd7b5008620ff911355c0066dcf7af26e2
Felipe Possari <fcpfelipe@hotmail.com>
2021-11-29 21:22:35 -0300

    Update validation log

--- src/main/java/com/felipepossari/jersey/adapter/in/web/food/v1/FoodRequestValidator.java
+++ src/main/java/com/felipepossari/jersey/adapter/in/web/food/v1/FoodRequestValidator.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import static com.felipepossari.jersey.adapter.in.web.food.v1.exception.FoodApiErrorReason.*;
+
 @Service
 public class FoodRequestValidator {
 
@@ -28,33 +30,33 @@ public class FoodRequestValidator {
     }
 
     private void validateFoodType(FoodRequest request, List<FoodApiErrorReason> errors) {
-        if(ObjectUtils.isEmpty(request.getType())){
-            log.warning("Food type null or empty");
-            errors.add(FoodApiErrorReason.FOOD_TYPE_INVALID);
-        }else{
-            try{
+        if (ObjectUtils.isEmpty(request.getType())) {
+            log.warning(FOOD_TYPE_INVALID.getMessage());
+            errors.add(FOOD_TYPE_INVALID);
+        } else {
+            try {
                 FoodType.valueOf(request.getType());
-            }catch (IllegalArgumentException ex){
-                log.warning("Food type invalid");
-                errors.add(FoodApiErrorReason.FOOD_TYPE_INVALID);
+            } catch (IllegalArgumentException ex) {
+                log.warning(FOOD_TYPE_INVALID.getMessage());
+                errors.add(FOOD_TYPE_INVALID);
             }
         }
     }
 
     private void validateWeight(FoodRequest request, List<FoodApiErrorReason> errors) {
         if (request.getWeight() <= 0) {
-            log.warning("Food weight invalid");
-            errors.add(FoodApiErrorReason.FOOD_WEIGHT_INVALID);
+            log.warning(FOOD_WEIGHT_INVALID.getMessage());
+            errors.add(FOOD_WEIGHT_INVALID);
         }
     }
 
     private void validateName(FoodRequest request, List<FoodApiErrorReason> errors) {
         if (StringUtils.isEmpty(request.getName())) {
-            log.warning("Food name empty or null");
-            errors.add(FoodApiErrorReason.FOOD_NAME_EMPTY);
+            log.warning(FOOD_NAME_EMPTY.getMessage());
+            errors.add(FOOD_NAME_EMPTY);
         } else if (request.getName().length() > FOOD_NAME_MAX_LENGTH) {
-            log.warning("Food name length invalid");
-            errors.add(FoodApiErrorReason.FOOD_NAME_INVALID_LENGTH);
+            log.warning(FOOD_NAME_INVALID_LENGTH.getMessage());
+            errors.add(FOOD_NAME_INVALID_LENGTH);
         }
     }
 }
