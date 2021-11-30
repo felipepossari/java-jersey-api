37feec18a561b24a9a930e66459fcb978f7406a0
Felipe Possari <fcpfelipe@hotmail.com>
2021-11-30 07:23:38 -0300

    Add FoodTestBuilder

---
+++ src/test/java/com/felipepossari/jersey/base/domain/FoodTestBuilder.java
@@ -0,0 +1,49 @@
+package com.felipepossari.jersey.base.domain;
+
+import com.felipepossari.jersey.application.domain.Food;
+import com.felipepossari.jersey.application.domain.FoodType;
+import com.felipepossari.jersey.base.DefaultConstants;
+
+public class FoodTestBuilder {
+
+    private String id = DefaultConstants.FOOD_ID;
+    private String name = DefaultConstants.FOOD_NAME;
+    private double weight = DefaultConstants.FOOD_WEIGHT;
+    private FoodType type = DefaultConstants.FOOD_TYPE;
+
+    private FoodTestBuilder() {
+    }
+
+    public static FoodTestBuilder aFoodRequest() {
+        return new FoodTestBuilder();
+    }
+
+    public FoodTestBuilder id(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public FoodTestBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public FoodTestBuilder weight(double weight) {
+        this.weight = weight;
+        return this;
+    }
+
+    public FoodTestBuilder type(FoodType type) {
+        this.type = type;
+        return this;
+    }
+
+    public Food build() {
+        return Food.builder()
+                .id(id)
+                .name(name)
+                .type(type)
+                .weight(weight)
+                .build();
+    }
+}
