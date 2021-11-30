2602b1ac5554fbfeab83989a0480df6ebd970600
Felipe Possari <fcpfelipe@hotmail.com>
2021-11-30 08:27:33 -0300

    Update ReadFoodUseCaseServiceTest

---
+++ src/test/java/com/felipepossari/jersey/application/service/ReadFoodUseCaseServiceTest.java
@@ -0,0 +1,82 @@
+package com.felipepossari.jersey.application.service;
+
+import com.felipepossari.jersey.application.domain.Food;
+import com.felipepossari.jersey.application.domain.FoodType;
+import com.felipepossari.jersey.application.exception.ApplicationErrorReason;
+import com.felipepossari.jersey.application.exception.ResourceNotFoundException;
+import com.felipepossari.jersey.application.port.out.FoodRepositoryPort;
+import com.felipepossari.jersey.base.domain.FoodTestBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static com.felipepossari.jersey.base.DefaultConstants.FOOD_ID;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ReadFoodUseCaseServiceTest {
+
+    @Mock
+    FoodRepositoryPort repositoryPort;
+
+    @InjectMocks
+    ReadFoodUseCaseService service;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "FROZEN FOOD"})
+    void readAllShouldReadAllFoodsIfTypeIsInvalid(String type) {
+        List<Food> foods = Collections.singletonList(FoodTestBuilder.aFood().build());
+
+        given(repositoryPort.readAll()).willReturn(foods);
+
+        List<Food> actualFoods = service.readAll(type);
+
+        verify(repositoryPort, times(0)).readByType(any(FoodType.class));
+    }
+
+    @Test
+    void readAllShouldReadFoodsByTypeIfTypeIsValid() {
+        FoodType foodType = FoodType.MEAT;
+        List<Food> foods = Collections.singletonList(FoodTestBuilder.aFood().build());
+
+        given(repositoryPort.readByType(foodType)).willReturn(foods);
+
+        List<Food> actualFoods = service.readAll(foodType.name());
+
+        verify(repositoryPort, times(0)).readAll();
+    }
+
+    @Test
+    void readByIdShouldReturnFoodIfItWasFound() {
+        Food food = FoodTestBuilder.aFood().build();
+
+        given(repositoryPort.readyById(FOOD_ID)).willReturn(Optional.of(food));
+
+        Food actualFood = service.readById(FOOD_ID);
+
+        Assertions.assertEquals(food, actualFood);
+    }
+
+    @Test
+    void readByIdShouldReturnThrowAResourceNotFoundExceptionIfItWasNotFound() {
+        given(repositoryPort.readyById(FOOD_ID)).willReturn(Optional.empty());
+
+        ResourceNotFoundException ex = Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> service.readById(FOOD_ID));
+
+        Assertions.assertEquals(ApplicationErrorReason.RESOURCE_NOT_FOUND, ex.getApplicationErrorReason());
+    }
+}
